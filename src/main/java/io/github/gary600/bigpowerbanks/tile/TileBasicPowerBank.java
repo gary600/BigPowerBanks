06303e6d29d8199747049d8c85e138f0d229325e
gary600 <gary600playsmc@gmail.com>
2017-04-01 22:01:42 -0600

    Actually follow the tutorial on the Tesla github for attaching capabilities

--- src/main/java/io/github/gary600/bigpowerbanks/tile/TileBasicPowerBank.java
+++ src/main/java/io/github/gary600/bigpowerbanks/tile/TileBasicPowerBank.java
@@ -1,8 +1,12 @@
 package io.github.gary600.bigpowerbanks.tile;
 
+import io.github.gary600.bigpowerbanks.tesla.TeslaContainer;
 import net.darkhax.tesla.api.ITeslaConsumer;
 import net.darkhax.tesla.api.ITeslaHolder;
 import net.darkhax.tesla.api.ITeslaProducer;
+import net.darkhax.tesla.api.implementation.BaseTeslaContainer;
+import net.darkhax.tesla.api.implementation.BaseTeslaContainerProvider;
+import net.darkhax.tesla.capability.TeslaCapabilities;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
@@ -10,44 +14,39 @@ import net.minecraft.util.ITickable;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 
-public class TileBasicPowerBank extends TileBase {
-    @CapabilityInject(ITeslaHolder.class)
-    public static final Capability<ITeslaHolder> TESLA_HOLDER_CAPABILITY = null;
-    @CapabilityInject(ITeslaConsumer.class)
-    public static final Capability<ITeslaConsumer> TESLA_CONSUMER_CAPABILITY = null;
-    @CapabilityInject(ITeslaProducer.class)
-    public static final Capability<ITeslaProducer> TESLA_PRODUCER_CAPABILITY = null;
+public class TileBasicPowerBank extends TileBase implements ITickable{
 
-    private ITeslaHolder teslaHolderInstance = TESLA_HOLDER_CAPABILITY.getDefaultInstance();
-    private ITeslaConsumer teslaConsumerInstance = TESLA_CONSUMER_CAPABILITY.getDefaultInstance();
-    private ITeslaProducer teslaProducerInstance = TESLA_PRODUCER_CAPABILITY.getDefaultInstance();
+    private BaseTeslaContainer container = new BaseTeslaContainer(0, 1000000, 1000, 1000);
 
     @Override
     public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
-        return TESLA_HOLDER_CAPABILITY == capability;
+        return TeslaCapabilities.CAPABILITY_HOLDER == capability
+                || TeslaCapabilities.CAPABILITY_CONSUMER == capability
+                || TeslaCapabilities.CAPABILITY_PRODUCER == capability;
     }
 
     @Override
     public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
-        if (capability == TESLA_HOLDER_CAPABILITY) return TESLA_HOLDER_CAPABILITY.<T> cast(teslaHolderInstance);
-        else if (capability == TESLA_CONSUMER_CAPABILITY) return TESLA_CONSUMER_CAPABILITY.<T> cast(teslaConsumerInstance);
-        else if (capability == TESLA_PRODUCER_CAPABILITY) return TESLA_PRODUCER_CAPABILITY.<T> cast(teslaProducerInstance);
-        else return null;
+        if (capability == TeslaCapabilities.CAPABILITY_CONSUMER
+                || capability == TeslaCapabilities.CAPABILITY_PRODUCER
+                || capability == TeslaCapabilities.CAPABILITY_HOLDER)
+            return (T) this.container;
+
+        return null;
     }
 
     @Override
     public NBTTagCompound serializeNBT() {
-        NBTTagCompound nbt = new NBTTagCompound();
-        nbt.setTag("teslaHolder", TESLA_HOLDER_CAPABILITY.getStorage().writeNBT(TESLA_HOLDER_CAPABILITY, teslaHolderInstance, null));
-        nbt.setTag("teslaConsumer", TESLA_CONSUMER_CAPABILITY.getStorage().writeNBT(TESLA_CONSUMER_CAPABILITY, teslaConsumerInstance, null));
-        nbt.setTag("teslaProducer", TESLA_PRODUCER_CAPABILITY.getStorage().writeNBT(TESLA_PRODUCER_CAPABILITY, teslaProducerInstance, null));
-        return nbt;
+        return container.serializeNBT();
     }
 
     @Override
     public void deserializeNBT(NBTTagCompound nbt) {
-        TESLA_HOLDER_CAPABILITY.getStorage().readNBT(TESLA_HOLDER_CAPABILITY, teslaHolderInstance, null, nbt.getTag("teslaHolder"));
-        TESLA_CONSUMER_CAPABILITY.getStorage().readNBT(TESLA_CONSUMER_CAPABILITY, teslaConsumerInstance, null, nbt.getTag("teslaConsumer"));
-        TESLA_PRODUCER_CAPABILITY.getStorage().readNBT(TESLA_PRODUCER_CAPABILITY, teslaProducerInstance, null, nbt.getTag("teslaProducer"));
+        container.deserializeNBT(nbt);
+    }
+
+    @Override
+    public void update() {
+        System.out.println(container.getStoredPower());
     }
 }
